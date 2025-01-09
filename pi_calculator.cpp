be93b344941586fdcffc0131c0c67a7ba840e870
Stygus <kuba.tokarczyk@onet.eu>
2025-01-09 21:46:06 +0100

    Pierwsza funkcja obliczeniowa

--- pi_calculator.cpp
+++ pi_calculator.cpp
@@ -5,4 +5,9 @@
 double calculate_partial_sum(int start, int end, int num_intervals) {
     double sum = 0.0;
     double step = 1.0 / num_intervals;
+    for (int i = start; i < end; ++i) {
+        double x = (i + 0.5) * step;
+        sum += 4.0 / (1.0 + x * x);
+    }
+    return sum;
 }
