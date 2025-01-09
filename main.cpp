6a0f072113998091c43b8f724c9af2cdf87ea6f9
Stygus <kuba.tokarczyk@onet.eu>
2025-01-09 21:40:36 +0100

    Ustawienie danych

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <thread>
+#include <chrono>
+#include "pi_calculator.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    std::vector<long long int> accuracies = {100000000, 1000000000, 3000000000};
+    std::ofstream outfile("pi_result.csv", std::ios_base::app);
+    outfile << "Dokladnosc,Watki,PI,Czas" << std::endl;
 }
