6c1a7fbf906857463c4ca88fa9d292130efbb1e7
Stygus <kuba.tokarczyk@onet.eu>
2025-01-09 21:44:46 +0100

    Dodanie pełnej funkcjonalości do main

--- main.cpp
+++ main.cpp
@@ -9,14 +9,18 @@ int main() {
     std::vector<long long int> accuracies = {100000000, 1000000000, 3000000000};
     std::ofstream outfile("pi_result.csv", std::ios_base::app);
     outfile << "Dokladnosc,Watki,PI,Czas" << std::endl;
+
     for (const auto& accuracy : accuracies) {
         for (int num_threads = 1; num_threads <= 50; ++num_threads) {
             auto start = std::chrono::high_resolution_clock::now();
             double pi = calculate_pi(accuracy, num_threads);
             auto end = std::chrono::high_resolution_clock::now();
             std::chrono::duration<double> duration = end - start;
-    
+
+            std::cout << "Dokladnosc: " << accuracy << ", Watki: " << num_threads << ", PI: " << pi << ", Czas: " << duration.count() << " sekund" << std::endl;
+            outfile << accuracy << "," << num_threads << "," << pi << "," << duration.count() << std::endl;
         }
     }
+
     return 0;
 }
